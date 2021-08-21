22654019d0fc809bdab761b52af5ca239a14df14
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-20 22:31:06 -0300

    Inserindo o nome da escola no comando WriteLine

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nome_escola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ETEC Adolpho Berezin");
+        }
+    }
+}
